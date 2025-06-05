4d592cf5992d3eb369493e991a220cf03fdf910f
kubotama <16697573+kubotama@users.noreply.github.com>
2025-06-05 22:24:26 +0900

    fix: エラーメッセージの表示を修正し、スタックトレースを削除

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -51,7 +51,8 @@ const Popup = () => {
         }
       })
       .catch((error) => {
-        setMessageText(`${error.name}: ${error.message} ${error.stack}}`);
+        setMessageText(`${error.name}: ${error.message}}`);
+        console.error(error);
       });
   };
 
