1c85156629b9b3c1f8444b3540d072e61b463d72
kubotama <16697573+kubotama@users.noreply.github.com>
2025-10-13 12:01:52 +0900

    refactor(options): useEffect内の不要なコメントを削除

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -15,13 +15,6 @@ const Options = () => {
 
   // コンポーネントのマウント時にストレージからURLを読み込む
   useEffect(() => {
-    // const loadUrl = async () => {
-    //   const data = await chrome.storage.local.get(STORAGE_KEY_BOOKMARK_URL);
-    //   if (data.bookmarkUrl) {
-    //     setUrl(data.bookmarkUrl);
-    //   }
-    // };
-    // loadUrl();
     (async () => {
       const data = await chrome.storage.local.get(STORAGE_KEY_BOOKMARK_URL);
       if (data.bookmarkUrl) {
